5c61c9ea5284cf7836ae8631a2ca4d0df21ed56c
voffan <vv.everstov@gmail.com>
2024-02-26 15:32:42 +0900

    added more tests

--- factor.py
+++ factor.py
@@ -1,9 +1,12 @@
 def factor(n):
     if not isinstance(n, int):
         raise TypeError()
+    if n <= 1:
+        raise Exception()
     d = {}
     m = int(n**0.5)
-    for i in range(2,m+1):
+    i = 2
+    while i <= m+1:
         prime = True
         for j in range(2,int(i**0.5)+1):
             if i % j == 0:
@@ -14,4 +17,5 @@ def factor(n):
             while n % i == 0:
                 d[i] += 1
                 n = n // i
+        i += 1
     return d
