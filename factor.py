b8410dc3e5abdc3d067aba593fd4f0d16d9e90d1
voffan <vv.everstov@gmail.com>
2024-04-01 14:26:19 +0900

    Refactor program: added is_prime function

--- factor.py
+++ factor.py
@@ -1,3 +1,12 @@
+def is_prime(n):
+    prime = True
+    for j in range(2,int(i**0.5)+1):
+        if i % j == 0:
+            prime = False
+            break
+    return prime
+
+
 def factor(n):
     if not isinstance(n, int):
         raise TypeError()
@@ -6,13 +15,8 @@ def factor(n):
     d = {}
     m = int(n**0.5)
     i = 2
-    while i <= m+1:
-        prime = True
-        for j in range(2,int(i**0.5)+1):
-            if i % j == 0:
-                prime = False
-                break
-        if n % i == 0 and prime:
+    while i <= m+1:        
+        if n % i == 0 and is_prime(i):
             d[i] = 0
             while n % i == 0:
                 d[i] += 1
